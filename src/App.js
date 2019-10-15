ba1cc7236687e09b487e7dbbdc07b81acd211c71
John Lewis <johnlewis@MacBook-Pro.local>
2019-10-15 16:47:08 -0600

    initial

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="row">Home</header>
+      <div className="body">
+        <div className="box"></div>
+        <p className='text-1'>Previous</p>
+        <p className='text-2'>Next</p>
+        
+      </div>
+      
+        
+      
+    </div>
+  );
+}
+
+export default App;
